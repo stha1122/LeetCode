38de32f91be4bdec24d5a4da9ba49e4647326a41
Shwetha S K <122188963+stha1122@users.noreply.github.com>
2023-10-01 18:26:46 +0530

    Time: 0 ms (100%), Space: 44.3 MB (64.67%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,21 +1,32 @@
+// class Solution {
+//     public int[][] matrixReshape(int[][] mat, int r, int c) {
+//         if((mat.length*mat[0].length)!=(r*c)){
+//             return mat;
+//         }
+//         int [][] result = new int [r][c];
+//         int res_col=0;
+//         int res_row=0;
+//         for(int i=0;i<mat.length;i++){
+//             for(int j=0;j<mat[0].length;j++){
+//                 if(res_col==c){
+//                    res_col=0;
+//                    res_row++; 
+//                 }
+//                 result[res_row][res_col]=mat[i][j];
+//                  res_col++;
+//             }
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
-    public int[][] matrixReshape(int[][] mat, int r, int c) {
-        if((mat.length*mat[0].length)!=(r*c)){
-            return mat;
-        }
-        int [][] result = new int [r][c];
-        int res_col=0;
-        int res_row=0;
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                if(res_col==c){
-                   res_col=0;
-                   res_row++; 
-                }
-                result[res_row][res_col]=mat[i][j];
-                 res_col++;
-            }
-        }
-        return result;
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int n = nums.length, m = nums[0].length;
+        if (r * c != n * m) return nums;
+        int[][] res = new int[r][c];
+        for (int i = 0; i < r * c; i++)
+            res[i / c][i % c] = nums[i / m][i % m];
+        return res;
     }
 }
