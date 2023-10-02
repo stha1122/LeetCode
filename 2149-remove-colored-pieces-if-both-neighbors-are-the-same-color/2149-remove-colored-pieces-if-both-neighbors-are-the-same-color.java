084e7e40620193a0523c6293660765ca4c47476c
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-10-02 16:05:55 +0530

    Time: 17 ms (36.91%), Space: 44 MB (68.76%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,16 +1,18 @@
 public class Solution {
     public boolean winnerOfGame(String colors) {
-        int countA = 0;
-        int countB = 0;
+        int countA = 0; // Count of valid moves for Alice
+        int countB = 0; // Count of valid moves for Bob
+        
+        // Iterate through the string and count valid moves
         for (int i = 1; i < colors.length() - 1; i++) {
-            if (colors.charAt(i - 1) == colors.charAt(i) && colors.charAt(i) == colors.charAt(i + 1)) {
-                if (colors.charAt(i) == 'A') {
-                    countA++;
-                } else {
-                    countB++;
-                }
+            if (colors.charAt(i - 1) == 'A' && colors.charAt(i) == 'A' && colors.charAt(i + 1) == 'A') {
+                countA++;
+            } else if (colors.charAt(i - 1) == 'B' && colors.charAt(i) == 'B' && colors.charAt(i + 1) == 'B') {
+                countB++;
             }
         }
+        
+        // Alice wins if she has more valid moves
         return countA > countB;
     }
 }
