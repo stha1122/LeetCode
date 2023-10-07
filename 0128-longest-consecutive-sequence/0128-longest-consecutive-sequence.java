f9a2d3a22ea7adb12efab262dfb877d6a3fa1f48
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-10-07 16:08:11 +0530

    Time: 14 ms (97.89%), Space: 56.9 MB (58.14%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -22,3 +22,29 @@ class Solution {
         return Math.max(longestStreak,currentStreak); //output is determined by which of the 2 is greater.
     }
 }
+
+
+
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int ali : nums){
+//             set.add(ali);
+//         }
+//         int max=0;
+//         for(int ali:nums){
+//             if(!set.contains(ali-1)){
+//                 int nn=ali;
+            
+//             int count =0;
+
+//             while(set.contains(nn)){
+//                 count++;
+//                 nn++;
+//             }
+//            max= Math.max(count,max);
+//         }
+//         }
+//         return max;
+//     }
+// }
