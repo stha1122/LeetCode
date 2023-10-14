51c16b21c8c6e33ff7281dc05dfa8fa18741618c
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-10-14 23:40:17 +0530

    Time: 2 ms (100%), Space: 54.3 MB (59.04%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,17 +1,17 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int sm=0;
-        for(int i=0;i<k;i++) sm+=nums[i];
-        int i=k;
-        int j=0;
-        double avg=sm/(double)k;
-        while(i<nums.length){
-            sm+=nums[i];
-            sm-=nums[j];
-            avg=Math.max(avg,sm/(double)k);
-            i++;
-            j++;
-        }
-        return avg;
+       int max_sum=Integer.MIN_VALUE;
+       int sum=0;
+       for(int i=0;i<k;i++){
+           sum+=nums[i];
+       }
+        max_sum = Math.max(sum,max_sum);
+       int s=0,e=k;
+       for(int i=e;i<nums.length;i++){
+           sum=sum+nums[i]-nums[s];
+           max_sum = Math.max(sum,max_sum);
+            s++;
+       }
+       return (double)max_sum/k;
     }
 }
