fb2258e91fb14e63ab5b08f8d375dcd3ccc55b36
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-15 20:31:01 +0530

    Time: 6 ms (98.68%) | Memory: 43.1 MB (94.21%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -1,21 +1,29 @@
 class Solution {
+    static final int LIMIT = 500;
+    
     public int longestArithSeqLength(int[] nums) {
-        int n = nums.length;
-        if (n <= 2)
-            return n;
-
-        int longest = 2;
-        Map<Integer, Integer>[] dp = new HashMap[n];
-
-        for (int i = 0; i < n; i++) {
-            dp[i] = new HashMap<>();
-            for (int j = 0; j < i; j++) {
-                int diff = nums[i] - nums[j];
-                dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
-                longest = Math.max(longest, dp[i].get(diff));
+        int maxSeqLen = 2;
+        int maxNum = nums[0];
+        int minNum = nums[0];
+        for (int num : nums) {
+            if (num > maxNum)  maxNum = num;
+            if (num < minNum)  minNum = num;
+        }
+        int maxDiff = maxNum - minNum;
+        int[] dpUp = new int[maxNum + 1];
+        int[] dpDown = new int[maxNum + 1];
+        for (int k = 0; k * maxSeqLen <= maxDiff; k++) {
+            Arrays.fill(dpUp, 0);
+            Arrays.fill(dpDown, 0);
+            for (int num : nums) {
+                if (maxSeqLen < (dpUp[num] = (num + k <= maxNum) ? 
+                                 (dpUp[num + k] + 1) : 1))
+                    maxSeqLen = dpUp[num];
+                if (maxSeqLen < (dpDown[num] = (num - k >= 0) ? 
+                                 (dpDown[num - k] + 1) : 1))
+                    maxSeqLen = dpDown[num];
             }
         }
-
-        return longest;
+        return maxSeqLen;
     }
 }
