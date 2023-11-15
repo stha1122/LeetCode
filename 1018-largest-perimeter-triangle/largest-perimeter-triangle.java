4b23b62a785d8920f362a677e1fc5d067be14429
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-15 23:09:26 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (17.89%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
@@ -1,15 +1,41 @@
 class Solution {
-     public static int largestPerimeter(int[] nums) {
+    public int largestPerimeter(int[] nums) {
        
-        Arrays.sort(nums);
         int n = nums.length;
-        for(int i=n-3;i>=0;i--) {
-        	if(nums[i]+nums[i+1]>nums[i+2]) {
-        		return(nums[i]+nums[i+1]+nums[i+2]);
-        	}
+        if(n == 3){
+            if(nums[1] + nums[2] > nums[0] && nums[0] + nums[2] > nums[1] && nums[0] + nums[1] >nums[2]){
+                return nums[0] + nums[1] + nums[2];
+            }
+            else{
+                return 0;
+            }
+        }
+        swap(nums, n-1);
+        swap(nums, n-2);
+        swap(nums, n-3);
+
+        for(int i = n-1; i>=2; i--){
+            if(nums[i] < nums[i-1] + nums[i-2]){
+                return nums[i] + nums[i-1] + nums[i-2];
+            }
+            else if(i>2){
+                swap(nums, i-3);
+            }
         }
         return 0;
-        
-	 
+    }
+
+
+    public void swap(int[] nums, int index){
+        int max = 0, maxIndex = -1;
+        for(int i = 0; i<=index; i++){
+            if(max < nums[i]){
+                max = nums[i];
+                maxIndex = i;
+            }
+        }
+        int temp = nums[index];
+        nums[index] = max;
+        nums[maxIndex] = temp;
     }
 }
