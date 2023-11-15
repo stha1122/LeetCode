a3a11b9a4745d8b9e27f09eabdfcaad7dc9486e9
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-15 13:26:57 +0530

    Time: 5 ms (99.11%) | Memory: 58.8 MB (69.03%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
@@ -1,53 +1,36 @@
 class Solution {
-    public int[] findOriginalArray(int[] changed) {
-        int n = changed.length;
-        if(n%2==1) return new int[0];
+    public int[] findOriginalArray(int[] c) {
+        int n = c.length;
+        int max = 0;
 
-        Arrays.sort(changed);
-        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            max = Math.max(max, c[i]);
+        }
 
-        for(int val : changed) add(map,val);
+        int[] sorted = new int[max+1];
+
+        for(int i=0; i<n; i++){
+            sorted[c[i]]++;
+        }
+        
         int[] ans = new int[n/2];
-        int j=0;
-        for(int val : changed){
-            if(val ==0 ){
-                if(map.containsKey(0) && map.get(0)>=2){
-                    ans[j] = 0;
-                    j++;
-                    remove(map,0);
-                    remove(map,0);
+        int itr=0;
+
+        if(sorted[0]%2 == 1)    
+            return new int[0];
+            
+        for(int i=1; i<=max; i++){
+            if(sorted[i] > 0){
+                if(2*i > max || sorted[i] > sorted[2*i])
+                    return new int[0];
+                else{
+                    sorted[2*i]-=sorted[i];
+                    for(int j=1; j<=sorted[i]; j++)
+                        ans[itr++] = i;
                 }
             }
-            else{
-                if(map.containsKey(val) && map.containsKey(val*2)){
-                    ans[j] = val;
-                    j++;
-                    remove(map,val);
-                    remove(map,val*2);
-
-                }
-            }
-
         }
-        if(j<n/2) return new int[0];
+
         return ans;
-    }
-
-    public static void add(HashMap<Integer,Integer> map,int key){
-        if(map.containsKey(key)){
-            map.put(key,map.get(key)+1);
-        }
-        else {
-            map.put(key,1);
-        }
-    }
-
-    public static void remove(HashMap<Integer,Integer> map,int key){
-        if(map.get(key)==1){
-            map.remove(key);
-        }
-        else{
-            map.put(key,map.get(key)-1);
-        }
     }
 }
