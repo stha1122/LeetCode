a10bfc05a535773daeb228bd8786bec383ea2d5f
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-15 13:25:16 +0530

    Time: 113 ms (31.12%) | Memory: 62.3 MB (8.85%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
@@ -1,18 +1,53 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
-        if(changed.length%2 != 0)
-            return new int[]{};
+        int n = changed.length;
+        if(n%2==1) return new int[0];
+
         Arrays.sort(changed);
-        Queue<Integer> queue = new LinkedList<>();
-        int[] res = new int[changed.length/2];
-        int i = 0;
-        for(int ele : changed){
-            if(!queue.isEmpty() && queue.peek() == ele){
-                res[i++] = queue.poll()/2;
-            }else{
-                queue.add(ele*2);
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int val : changed) add(map,val);
+        int[] ans = new int[n/2];
+        int j=0;
+        for(int val : changed){
+            if(val ==0 ){
+                if(map.containsKey(0) && map.get(0)>=2){
+                    ans[j] = 0;
+                    j++;
+                    remove(map,0);
+                    remove(map,0);
+                }
             }
+            else{
+                if(map.containsKey(val) && map.containsKey(val*2)){
+                    ans[j] = val;
+                    j++;
+                    remove(map,val);
+                    remove(map,val*2);
+
+                }
+            }
+
         }
-        return (queue.size() == 0)?res: new int[]{};
+        if(j<n/2) return new int[0];
+        return ans;
+    }
+
+    public static void add(HashMap<Integer,Integer> map,int key){
+        if(map.containsKey(key)){
+            map.put(key,map.get(key)+1);
+        }
+        else {
+            map.put(key,1);
+        }
+    }
+
+    public static void remove(HashMap<Integer,Integer> map,int key){
+        if(map.get(key)==1){
+            map.remove(key);
+        }
+        else{
+            map.put(key,map.get(key)-1);
+        }
     }
 }
