f64adb20c030d2b6be2e08c7ee5834fc67d465ef
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-16 07:58:53 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (36.64%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,23 +1,41 @@
+// class Solution {
+//     public int[] findErrorNums(int[] nums) {
+//         // Arrays.sort(nums);
+//         for(int i=0; i<nums.length; i++){
+//             while(nums[i] != i+1 && nums[nums[i] - 1] != nums[i]){
+//                 swap(nums, i, nums[i]-1);
+//             }
+//         }
+//         for(int i=0; i<nums.length; i++){
+//             if(nums[i] != i+1){
+//                 return new int[]{nums[i], i + 1};
+//             }
+//         }
+//         return null;
+//     }
+
+//     private void swap(int[] nums, int i, int j){
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
+
 class Solution {
-   public int[] findErrorNums(int[] nums) {
-        int ans[]=new int[2];
-        
-        for(int i=0;i<nums.length;i++) {
-           
-        	if(nums[Math.abs(nums[i])-1]<0) {
-        		ans[0]=Math.abs(nums[i]);
-        	}
-        	else {
-        		nums[Math.abs(nums[i])-1]*=(-1);
-        	}
+    public int[] findErrorNums(int[] nums) {
+        boolean[] n = new boolean[nums.length+1];
+        int[] ans = new int[2];
+        for(int num : nums){
+            if(n[num]){
+                ans[0] = num;
+            }
+            n[num] = true;
         }
-        
-        for(int i=0;i<nums.length;i++) {
-        	if(nums[i]>0) {
-        		ans[1]=i+1;
-        		break;
-        	}
+        for(int i=1; i<=nums.length; i++){
+            if(!n[i]){
+                ans[1]=i;
+            }
         }
         return ans;
-      }
+    }
 }
