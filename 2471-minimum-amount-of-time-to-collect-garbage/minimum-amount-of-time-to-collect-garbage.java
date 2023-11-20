aa3cf26601908c1f48ae58f692bb81288be857f7
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-20 21:24:30 +0530

    Time: 2 ms (100.00%) | Memory: 61 MB (71.78%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -1,38 +1,36 @@
 class Solution {
+    boolean hasG = false, hasP = false, hasM = false;
 
-    public int calculateTime(int travel[], HashMap<Integer,Integer>garbage){
-        if(garbage.size()==0)
-            return 0;
-        int i=0,ans=0;
-        for(Map.Entry<Integer,Integer>x:garbage.entrySet()){
-            int garbage_loc = x.getKey();
-            int garbage_qty = x.getValue();
-            while(garbage_loc!=0 && i<garbage_loc){
-                ans+=travel[i];
-                i++;
+    public int garbageCollection(String[] garbage, int[] travel) {
+        int n = garbage.length;
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            ans += 3 * travel[i];
+        }
+        for (String s : garbage) {
+            ans += s.length();
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("G")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
             }
-            ans+=garbage_qty;
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("P")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("M")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
         }
         return ans;
-    }
-    public int garbageCollection(String[] garbage, int[] travel) {
-        HashMap<Integer,Integer>metal = new HashMap<>();
-        HashMap<Integer,Integer>paper = new HashMap<>();
-        HashMap<Integer,Integer>glass = new HashMap<>();
-
-        for(int i=0;i<garbage.length;i++){
-            String curr_word = garbage[i];
-            for(int j=0;j<curr_word.length();j++){
-                char curr_char = curr_word.charAt(j);
-                if(curr_char == 'M')
-                    metal.put(i,metal.getOrDefault(i,0)+1);
-                else if(curr_char == 'P')
-                    paper.put(i,paper.getOrDefault(i,0)+1);
-                else if(curr_char == 'G')
-                    glass.put(i,glass.getOrDefault(i,0)+1);
-            }
-        }
-
-        return calculateTime(travel,metal)+calculateTime(travel,paper)+calculateTime(travel,glass);
     }
 }
