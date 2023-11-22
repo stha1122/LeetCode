8a123189ada0714462b4e5f92179c286a11eb495
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-22 23:34:54 +0530

    Time: 20 ms (100.00%) | Memory: 54.2 MB (80.46%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.java
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.java
@@ -1,33 +1,29 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public int countNicePairs(int[] nums) {
-        final int MOD = 1_000_000_007;
-        int count = 0;
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
 
-        for (int num : nums) {
-            int diff = num - reverse(num);
-            frequencyMap.put(diff, frequencyMap.getOrDefault(diff, 0) + 1);
+    int reverse(int num) {
+        int rev = 0;
+        while(num>0){
+            rev = rev*10 + num%10;
+            num/=10;
         }
-
-        for (int freq : frequencyMap.values()) {
-            // For each frequency, add the number of nice pairs that can be formed
-            // The formula for the number of pairs is (freq * (freq - 1)) / 2
-            count = (int) ((count + (long) freq * (freq - 1) / 2) % MOD);
-        }
-
-        return count;
+        return rev;
     }
 
-    // Helper function to reverse an integer
-    private int reverse(int num) {
-        int reversed = 0;
-        while (num > 0) {
-            reversed = reversed * 10 + num % 10;
-            num /= 10;
+    public int countNicePairs(int[] nums) {
+        final int mod = 1000000007;
+        
+        int len = nums.length;
+        for(int i=0;i<len;i++) nums[i] = nums[i] - reverse(nums[i]);
+        Arrays.sort(nums);
+        long res = 0;
+        for(int i=0;i<len-1;i++) {
+            long cont = 1;
+            while(i<len-1 && nums[i]==nums[i+1]) {
+                cont++;
+                i++;
+            }
+            res = (res%mod + (cont*(cont-1))/2)%mod;
         }
-        return reversed;
+        return (int) res%mod;
     }
 }
