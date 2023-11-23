dcdec646ad5a9eb821f4666b5d53f484731f6007
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-23 09:18:13 +0530

    Time: 16 ms (94.51%) | Memory: 44.4 MB (49.73%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -1,37 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
-    public static List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        int q_count = l.length;
+  public static List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+    	int q_count = l.length;
         List<Boolean> ans = new ArrayList<>();
-
-        for (int i = 0; i < q_count; i++) {
-            int left = l[i];
-            int right = r[i];
-            int length = right - left + 1;
-            int[] arr = new int[length];
-            
-            // Copy elements from the specified range of nums to a new array (subarray)
-            System.arraycopy(nums, left, arr, 0, length);
-
-            ans.add(check(arr));
-        }
-        return ans;
+    	
+    	for(int i=0;i<q_count;i++) {
+    		int left = l[i];
+    		int right = r[i];
+    		int  length = right-left+1;
+    		int arr[] = new int[length];
+    		for(int k=0,m=left;k<length && m<=right;k++,m++) {
+    			arr[k]=nums[m];
+    		}
+    		
+    		ans.add(check(arr));
+    	}
+    	return ans;
+    	
+        
     }
-
-    private static boolean check(int[] arr) {
-        // Sort the subarray
-        Arrays.sort(arr);
-
-        int diff = arr[1] - arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] - arr[i - 1] != diff) {
-                return false;
-            }
-        }
-        return true;
-    }
+	private static boolean check(int[] arr) {
+		// TODO Auto-generated method stub
+		Arrays.sort(arr);
+		int diff = arr[1]-arr[0];
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]-arr[i-1] !=diff) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
