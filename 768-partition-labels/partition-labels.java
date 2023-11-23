99d524113699bb977e39e212eb7bff70a89b3d8f
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-23 19:31:36 +0530

    Time: 3 ms (97.66%) | Memory: 41.5 MB (36.61%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,37 +1,25 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        // List to store the sizes of partitions
         List<Integer> result = new ArrayList<>();
-
-        // Array to store the last occurrence index of each character
         int[] last = new int[26];
+        char[] str = s.toCharArray();
 
-        // Fill the 'last' array with the last occurrence index of each character
-        for (int i = 0; i < s.length(); i++) {
-            last[s.charAt(i) - 'a'] = i;
+        for (int i = 0; i < str.length; i++) {
+            last[str[i] - 'a'] = i;
         }
 
-        // Initialize pointers for the current partition's start and end
         int start = 0;
         int end = 0;
-
-        // Iterate through the string to find partition sizes
-        for (int i = 0; i < s.length(); i++) {
-            // Determine the last occurrence index of the current character
-            int currentEnd = last[s.charAt(i) - 'a'];
-
-            // Update 'end' to be the maximum of its current value and the last occurrence index of the current character
+        for (int i = 0; i < str.length; i++) {
+             int currentEnd = last[str[i] - 'a'];
             end = Math.max(end, currentEnd);
-
-            // If the current index is equal to 'end', it indicates the end of the current partition
-            // Add the length of the partition to the result list, and update 'start' to the next index
             if (i == end) {
                 result.add(end - start + 1);
-                start = i + 1;
+                start = i+1;
             }
-        }
 
-        // Return the list of partition sizes
-        return result;
+           
+        }
+    return result;
     }
 }
