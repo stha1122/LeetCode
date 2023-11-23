0b8cb08002f41f7380fa3a3580b84bdd478c7693
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-23 09:20:10 +0530

    Time: 16 ms (94.51%) | Memory: 44.4 MB (49.73%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -1,32 +1,46 @@
 class Solution {
-  public static List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-    	int q_count = l.length;
-        List<Boolean> ans = new ArrayList<>();
-    	
-    	for(int i=0;i<q_count;i++) {
-    		int left = l[i];
-    		int right = r[i];
-    		int  length = right-left+1;
-    		int arr[] = new int[length];
-    		for(int k=0,m=left;k<length && m<=right;k++,m++) {
-    			arr[k]=nums[m];
-    		}
-    		
-    		ans.add(check(arr));
-    	}
-    	return ans;
-    	
-        
+  // This function checks if the given array can be rearranged into an arithmetic sequence.
+  private static boolean check(int[] arr) {
+    // Sort the array in ascending order.
+    Arrays.sort(arr);
+
+    // Calculate the common difference between consecutive elements.
+    int diff = arr[1] - arr[0];
+
+    // Check if the differences between consecutive elements are consistent.
+    for(int i = 1; i < arr.length; i++) {
+      if(arr[i] - arr[i-1] != diff) {
+        return false;
+      }
     }
-	private static boolean check(int[] arr) {
-		// TODO Auto-generated method stub
-		Arrays.sort(arr);
-		int diff = arr[1]-arr[0];
-		for(int i=1;i<arr.length;i++) {
-			if(arr[i]-arr[i-1] !=diff) {
-				return false;
-			}
-		}
-		return true;
-	}
+    return true;
+  }
+
+  // This function processes queries to determine if corresponding subarrays can be rearranged into arithmetic sequences.
+  public static List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+    // Get the number of queries.
+    int q_count = l.length;
+    // Initialize a list to store the results.
+    List<Boolean> ans = new ArrayList<>();
+
+    // Iterate through each query.
+    for(int i = 0; i < q_count; i++) {
+      int left = l[i];
+      int right = r[i];
+      int length = right - left + 1;
+
+      // Extract the subarray from nums based on the current query.
+      int arr[] = new int[length];
+      for(int k = 0, m = left; k < length && m <= right; k++, m++) {
+        arr[k] = nums[m];
+      }
+
+      // Check if the subarray can be rearranged into an arithmetic sequence and add the result to the list.
+      ans.add(check(arr));
+    }
+
+    // Return the final list of results.
+    return ans;
+  }
 }
+
