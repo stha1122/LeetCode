c7dadf9290c85decf9c829becea73b611a379d25
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-24 06:46:22 +0530

    Time: 27 ms (98.94%) | Memory: 54.8 MB (55.20%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
@@ -2,7 +2,8 @@ class Solution {
     public int maxCoins(int[] piles) {
         Arrays.sort(piles);
         int sum=0,i=0,j=piles.length-2;
-        while(i++<piles.length/3){
+        int total_piles = piles.length/3;
+        while(i++<total_piles){
             sum+=piles[j];
             j-=2;
         }
