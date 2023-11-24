ac9aa8466a8a9ff7aa578c12e1103b9ef674dbfd
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-24 06:48:52 +0530

    Time: 29 ms (15.52%) | Memory: 54.8 MB (58.20%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.java
@@ -1,32 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
-    
-    // Define a method named maxCoins, which takes an array of integers (piles) as input and returns an integer
     public int maxCoins(int[] piles) {
-        
-        // Sort the array piles in ascending order
         Arrays.sort(piles);
-        
-        // Initialize variables to keep track of the sum of coins and pointers for array traversal
-        int sum = 0;          // to store the total sum of coins obtained
-        int i = 0;            // pointer for iterating through the array
-        int j = piles.length - 2;  // pointer starting from the second-to-last index
-        
-        // Calculate the total number of valid piles to consider (every third pile)
-        int total_piles = piles.length / 3;
-        
-        // Iterate through the array in steps of 2 (picking every third pile)
-        while (i++ < total_piles) {
-            
-            // Add the value at index j to the sum
-            sum += piles[j];
-            
-            // Update j by decrementing it by 2 to move to the next valid index
-            j -= 2;
+        int sum=0,i=0,j=piles.length-2;
+        int total_piles = piles.length/3;
+        while(i++<total_piles){
+            sum+=piles[j];
+            j-=2;
         }
-        
-        // Return the final sum, representing the maximum number of coins you can collect
         return sum;
     }
 }
