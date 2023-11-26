8c21df070ed003276c444c21a2e39f87388f34dc
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-11-26 23:12:41 +0530

    Time: 8 ms (100.00%) | Memory: 67.7 MB (45.78%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -1,39 +1,21 @@
 class Solution {
     public int largestSubmatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        List<Pair<Integer, Integer>> prevHeights = new ArrayList<>();
-        int result = 0;
-
-        for (int row = 0; row < m; row++) {
-            List<Pair<Integer, Integer>> heights = new ArrayList<>();
-            boolean[] seen = new boolean[n];
-
-            for (Pair<Integer, Integer> entry : prevHeights) {
-                int height = entry.getKey();
-                int col = entry.getValue();
-                if (matrix[row][col] == 1) {
-                    heights.add(new Pair<>(height + 1, col));
-                    seen[col] = true;
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 1; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] = matrix[i - 1][j] + 1;
                 }
             }
-
-            for (int col = 0; col < n; col++) {
-                if (!seen[col] && matrix[row][col] == 1) {
-                    heights.add(new Pair<>(1, col));
-                }
-            }
-
-            for (int i = 0; i < heights.size(); i++) {
-                int base = i + 1;
-                int height = heights.get(i).getKey();
-                result = Math.max(result, base * height);
-            }
-
-            prevHeights = heights;
         }
-
-        return result;
+        int ans = 0;
+        for (var row : matrix) {
+            Arrays.sort(row);
+            for (int j = n - 1, k = 1; j >= 0 && row[j] > 0; --j, ++k) {
+                int s = row[j] * k;
+                ans = Math.max(ans, s);
+            }
+        }
+        return ans;
     }
-    
 }
