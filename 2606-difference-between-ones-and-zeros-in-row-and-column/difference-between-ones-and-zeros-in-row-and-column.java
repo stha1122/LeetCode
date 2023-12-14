6ee4e6a372db1228351c0071c90e8772d7cbf9c0
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-12-14 22:25:15 +0530

    Time: 3 ms (100.00%) | Memory: 73.8 MB (38.51%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[][] onesMinusZeros(int[][] grid) {
-       int onesRow [] = new int[grid.length];
-        int onesCol[] = new int[grid[0].length];
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                    if(grid[i][j]==1){
-                        onesRow[i]++;
-                        onesCol[j]++;
-                    }
+        int row[]= new int[grid.length];
+        int col[]= new int[grid[0].length];
+        for(int i=0; i<row.length; i++){
+            int one=0;
+            for(int j=0; j<col.length; j++){
+                one = one + grid[i][j];
+            }
+            row[i]= (2*one)-(row.length);
+        }
+
+        for(int j=0; j<col.length; j++){
+            int one=0;
+            for(int i=0; i<row.length; i++){
+                one = one + grid[i][j];
+            }
+            col[j]= (2*one)-(col.length);
+        }
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                grid[i][j]= row[i]+col[j];
             }
         }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                   grid[i][j] = (onesRow[i]+onesCol[j]-(grid.length-onesRow[i])-(grid[0].length-onesCol[j]));
-            }
-        }
-    return grid;
+        return grid;
     }
 }
