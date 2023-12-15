132e7d7d647dfeaa93c73228b7cc930a1b005be3
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-12-15 22:29:50 +0530

    Time: 2 ms (79.52%) | Memory: 43.2 MB (39.87%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -1,16 +1,18 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        Set<String> cities = new HashSet<>(); 
-        for (List<String> path : paths) {
-            cities.add(path.get(0)); 
-        }
         
+        HashSet<String> depart = new HashSet<>();
+     
+
         for (List<String> path : paths) {
-            String dest = path.get(1); 
-            if (!cities.contains(dest)) {
-                return dest; 
-            }
+            depart.add (path.get(0));
+        
         }
+
+        for (List<String> path : paths) {
+            if (!depart.contains(path.get(1))) return path.get(1);
+        }
+
         return "";
     }
 }
