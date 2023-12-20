bc66a33cee9e4de1666a62939e42840f2419ce34
stha1122 <122188963+stha1122@users.noreply.github.com>
2023-12-20 19:38:37 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (5.53%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.java
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -1,9 +1,20 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        if(prices[0]+prices[1]<=money){
-            return (money - (prices[0]+prices[1]));
+        int small;
+        int smaller;
+        if (prices[0] < prices[1]) {
+            small = prices[1];
+            smaller = prices[0];
+        } else {
+            small = prices[0];
+            smaller = prices[1];
         }
-        return money;
+        for (int i = 2; i < prices.length; i++) {
+            if (prices[i] < smaller) {
+                small = smaller;
+                smaller = prices[i];
+            } else if (prices[i] < small) small = prices[i];
+        }
+        return small + smaller <= money ? money - small - smaller : money;
     }
 }
