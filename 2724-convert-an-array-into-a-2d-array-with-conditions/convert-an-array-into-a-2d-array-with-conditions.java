5ab7c43fbaf043132b137dea010e2da7ebad2c25
stha1122 <122188963+stha1122@users.noreply.github.com>
2024-01-02 18:16:08 +0530

    Time: 9 ms (8.05%) | Memory: 44.4 MB (50.21%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,18 +1,31 @@
+
 class Solution {
-    public List<List<Integer>> findMatrix(int[] nums) {
-        int freq[] = new int[nums.length + 1];
-
-        ArrayList<List<Integer>> ans = new ArrayList<>();
-        for (int c : nums) {
-            if (freq[c] >= ans.size()) {
-                ans.add(new ArrayList<>());
-            }
-
-            // Store the integer in the list corresponding to its current frequency.
-            ans.get(freq[c]).add(c);
-            freq[c]++;
+    public List<List<Integer>> findMatrix(int[] v) {
+        Map<Integer, Integer> um = new HashMap<>();
+        for (int i : v) {
+            um.put(i, um.getOrDefault(i, 0) + 1);
         }
-
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        while (!um.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            List<Integer> toErase = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : um.entrySet()) {
+                int f = entry.getKey();
+                int s = entry.getValue();
+                temp.add(f);
+                s--;
+                if (s == 0) {
+                    toErase.add(f);
+                }
+                um.put(f, s);
+            }
+            ans.add(temp);
+            for (int i : toErase) {
+                um.remove(i);
+            }
+        }
         return ans;
     }
 }
+
