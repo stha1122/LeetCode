54be5b6d3dc3676553129d4be88b41afe82c5c05
stha1122 <122188963+stha1122@users.noreply.github.com>
2024-01-10 09:15:56 +0530

    Time: 7 ms (99.70%) | Memory: 77.4 MB (65.77%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -1,69 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    TreeNode infected;
-    
-    public int amountOfTime(TreeNode root, int start) {
-        Map<TreeNode, TreeNode> hm = new HashMap<>();
-        buildParent(root, null, hm, start);
-        
-        Queue<TreeNode> q = new ArrayDeque<>();
-        q.offer(infected);
-        
-        Set<Integer> seen = new HashSet<>();
-        
-        int minutes = 0;
-        
-        while (!q.isEmpty()) {
-            int size = q.size();
-            minutes++;
-            
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
-                // System.out.println(curr.val);
-                
-                seen.add(curr.val);
-                
-                if (curr.left != null && !seen.contains(curr.left.val)) 
-                    q.offer(curr.left);
-                
-                if (curr.right != null && !seen.contains(curr.right.val))
-                    q.offer(curr.right);
-                
-                if (hm.containsKey(curr) && hm.get(curr) != null && !seen.contains(hm.get(curr).val))
-                    q.offer(hm.get(curr));
-            }
-        }
-        
-        return minutes - 1;
+  private int ans;
+
+  public int amountOfTime(TreeNode root, int start) {
+    dfs(root, start);
+    return ans;
+  }
+
+  public int dfs(TreeNode root, int start) {
+    if (root == null) return 0;
+
+    int leftDepth = dfs(root.left, start);
+    int rightDepth = dfs(root.right, start);
+
+    if (root.val == start) {
+      ans = Math.max(leftDepth, rightDepth);
+      return -1;
+    } else if (leftDepth >= 0 && rightDepth >= 0) {
+      return Math.max(leftDepth, rightDepth) + 1;
+    } else {
+      ans = Math.max(ans, Math.abs(leftDepth - rightDepth));
+      return Math.min(leftDepth, rightDepth) - 1;
     }
-    
-    public void buildParent(TreeNode node, TreeNode parent, Map<TreeNode, TreeNode> hm, int start) {
-        if (node == null)
-            return;
-    
-        if (start == node.val) 
-            infected = node;
-        
-        hm.put(node, parent);
-        
-        if (node.left != null)
-            buildParent(node.left, node, hm, start);
-        
-        if (node.right != null)
-            buildParent(node.right, node, hm, start);
-    }
+  }
 }
