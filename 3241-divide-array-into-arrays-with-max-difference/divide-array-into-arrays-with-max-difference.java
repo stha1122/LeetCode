b567b58119ad0dcc73a8cf0db66a9242077e0fa1
stha1122 <122188963+stha1122@users.noreply.github.com>
2024-02-01 07:56:48 +0530

    Time: 24 ms (68.69%) | Memory: 59.3 MB (11.55%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -1,20 +1,22 @@
-public class Solution {
+class Solution {
     public int[][] divideArray(int[] nums, int k) {
+        int n = nums.length;
+        int[][] ans = new int[n/3][3];
         Arrays.sort(nums);
-        int[][] result = new int[0][0];
-        
-        for (int i = 0; i + 2 < nums.length; ++i) {
-            if (i % 3 == 0) {
-                if (nums[i + 2] - nums[i] <= k) {
-                    int[] triplet = {nums[i], nums[i + 1], nums[i + 2]};
-                    result = Arrays.copyOf(result, result.length + 1);
-                    result[result.length - 1] = triplet;
-                } else {
-                    return new int[0][0];
-                }
+        int idx = 0;
+        int curr = 2;
+        int prev = 1;
+        int prevp = 0;
+        int[][] a = new int[0][0];
+        for(int i = 0; i < n/3; i++){
+            if(nums[curr]-nums[prev] > k || nums[curr]-nums[prevp] > k || nums[prev]-nums[prevp] > k)return a;
+            for(int j = 0; j < 3; j++){
+                ans[i][j] = nums[idx++];
             }
+            curr += 3;
+            prev += 3;
+            prevp += 3;
         }
-        
-        return result;
+        return ans;
     }
 }
