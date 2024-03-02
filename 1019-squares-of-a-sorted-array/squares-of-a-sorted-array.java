d383b39bdc1de380533498f032372b9c7cc26410
stha1122 <122188963+stha1122@users.noreply.github.com>
2024-03-02 10:49:39 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (66.84%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,67 +1,20 @@
-
-class Solution {
+public class Solution {
     public int[] sortedSquares(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {   // Square each element in 'nums'.
+            nums[i] *= nums[i];
         }
-        Arrays.sort(nums);
-        return nums;
+        int i = n - 1;  //initial position of i 
+        int l = 0, r = n - 1;
+ // Use two pointers to merge sorted squares in non-decreasing order.
+        while (l <= r) {
+            if (nums[l] > nums[r]) {
+                ans[i--] = nums[l++];
+            } else {
+                ans[i--] = nums[r--];
+            }
+        }
+        return ans;
     }
 }
-
-
-
-
-// class Solution {
-//     public int[] sortedSquares(int[] nums) {
-//         for(int i=0;i<nums.length;i++){
-//             nums[i]= nums[i]*nums[i];
-//         }
-//         mergeSort(nums,0,nums.length-1);
-//         return nums;
-//     }
-
-//     public static void mergeSort(int arr[],int start,int end){
-//         if(start>=end){
-//             return;
-//         }
-        
-//         int mid = (start + end)/2;
-//         mergeSort(arr,start,mid);
-//         mergeSort(arr,mid+1,end);
-     
-//         merge(arr,start,mid,end);
-//     }
-
-//     public static void merge(int arr[],int start,int mid,int end){
-//         int temp[] = new int[end-start+1];
-//         int left = start;int right = mid+1;
-//         int i=0;
-//         while(left<=mid && right<=end){
-//             if(arr[left]<=arr[right]){
-//                 temp[i]=arr[left];
-//                 i++;
-//                 left++;
-//                     }
-//             else{
-//                  temp[i]=arr[right];
-//                 i++;
-//                 right++;
-//             }
-//         }
-//         while(left<=mid){
-//             temp[i++] = arr[left++];
-            
-//         }
-
-//          while(right<=end){
-//             temp[i++] = arr[right++];
-            
-//         }
-
-//           for (int j = 0; j < temp.length; j++) {
-//         arr[start + j] = temp[j];
-//     }
-    
-//     }
-// }
