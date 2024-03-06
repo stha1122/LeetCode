38a29e5f70b6c05406b2cceacdd31f3fc4ad7fcd
stha1122 <122188963+stha1122@users.noreply.github.com>
2024-03-06 19:28:24 +0530

    Time: 25 ms (55.92%) | Memory: 45.9 MB (86.91%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,32 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        return getSubarraySumCount(nums, k);
-    }
-
-    public int getSubarraySumCount(int[] nums, int k) {
-        int n = nums.length;
-        int count = 0;
         int sum = 0;
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
 
-        // Map to store the cumulative sum and its frequency
-        Map<Integer, Integer> sumFrequencyMap = new HashMap<>();
-        sumFrequencyMap.put(0, 1); // Initialize with 0 to handle subarrays starting from index 0
+        for(int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            int rem = sum - k;
 
-        for (int num : nums) {
-            sum += num;
-
-            // Check if (sum - k) is present in the map, indicating a subarray with sum k
-            if (sumFrequencyMap.containsKey(sum - k)) {
-                count += sumFrequencyMap.get(sum - k);
+            if(map.containsKey(rem)) {
+                count += map.get(rem);
             }
-
-            // Update the frequency of the current cumulative sum
-            sumFrequencyMap.put(sum, sumFrequencyMap.getOrDefault(sum, 0) + 1);
+            map.put(sum, map.getOrDefault(sum, 0)+1);
         }
-
         return count;
     }
 }
