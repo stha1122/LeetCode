fbc7967c58bb785e4d4e3e25749a693783556e55
stha1122 <122188963+stha1122@users.noreply.github.com>
2024-04-07 19:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (42.85%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int p1=0,p2=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                p1++;
+                p2++;
+            } else if(s.charAt(i)==')'){
+                p1--;
+                p2--;
+            } else if(s.charAt(i)=='*'){
+                p1++;
+                p2--;
+            }
+            if(p1<0){
+                return false;
+            }
+            if(p2<0){
+                p2 = 0;
+            }
+        }
+        return p2==0;
+    }
+}
