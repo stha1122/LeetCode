be5dae7c4477981b66a598599865ca17c7e53bbd
stha1122 <122188963+stha1122@users.noreply.github.com>
2024-11-26 06:23:02 +0530

    Time: 1 ms (76.63%) | Memory: 45.8 MB (19.57%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,42 +1,71 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int col0 = -1;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int m = matrix.length ;  // row size
+        int n = matrix[0].length ; // column size
+
+        boolean firstRowZero = false ;
+        boolean firstColumnZero = false ;
+
+        // marking firstColumn
+        for(int i=0;i<m;i++){
+            //checking first column
+            if(matrix[i][0]==0){
+                firstColumnZero = true ;
+            }
+        }
+        // marking firstRow 
+        for(int j=0;j<n;j++){
+
+            //checking first row
+            if(matrix[0][j]==0){
+                firstRowZero = true ;
+            }            
+        }
+
+        // checking inner matrix
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+
                 if(matrix[i][j]==0){
-                    matrix[i][0] = 0;
-                    if(j!=0) matrix[0][j] = 0;
-                    else col0 = 0;
-            }
+
+                    //mark the first column 
+                    matrix[0][j] = 0 ;
+
+                    //mark the first row
+                    matrix[i][0] = 0 ;
+                    
+                }
+            }            
         }
+
+        //convert to zeros based on first row,column
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+
+                // if first column    OR   first row is zero
+                if(matrix[0][j]==0 || matrix[i][0]==0 ){
+                     
+                    matrix[i][j] = 0 ;    //       
+                
+                }
+            }            
+        }
+
+        //convert first row
+        if(firstRowZero){
+            for(int j=0;j<n;j++){
+
+                matrix[0][j]=0 ;
+            }           
+        }
+
+        //convert first column
+        if(firstColumnZero){
+            for(int i=0;i<m;i++){
+
+                matrix[i][0]=0 ;
+            }           
+        }              
     }
-
-    
-         for(int i =1; i<n; i++)
-        {
-            for(int j = 1; j<m; j++)
-            {
-               if(matrix[i][j]!=0 && (matrix[i][0] == 0 || matrix[0][j]==0))
-               {
-                matrix[i][j] = 0;
-               }
-            }
-        }
-
-    
-        for(int j =m-1; j>=0; j--)
-        {
-            if(matrix[0][0]==0) matrix[0][j] = 0;
-        }
-
-          for(int i =n-1; i>=0; i--)
-        {
-            if(col0==0) matrix[i][0] = 0;
-        }
-
-    }
-
 }
